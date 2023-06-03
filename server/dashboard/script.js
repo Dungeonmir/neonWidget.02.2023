0942ebe2a341e50986d0ad41e972895d47f7bc98
Sergey <dungeonmir@gmail.com>
2023-06-03 21:06:28 +0300

    proper server for orders

--- server/dashboard/script.js
+++ server/dashboard/script.js
@@ -1,44 +1,42 @@
-
 let data = [
-    {
-      image: 'https://via.placeholder.com/500x500/cccccc/615f5f?text=Text',
-      width: '43.43',
-      height: '54.55',
-      price: '6543',
-      phone: '83434333',
-      elements: '7'
-    },
-    {
-        image: 'https://via.placeholder.com/500x500/cccccc/615f5f?text=Why',
-        width: '43.43',
-        height: '54.55',
-        price: '5664',
-        phone: '83434333',
-        elements: '7'
-      },
-      {
-        image: 'https://via.placeholder.com/500x500/cccccc/615f5f?text=Something',
-        width: '43.43',
-        height: '54.55',
-        price: '3644',
-        phone: '83434333',
-        elements: '20'
-      },
-  ];
+	{
+		image: "https://via.placeholder.com/500x500/cccccc/615f5f?text=Text",
+		width: "43.43",
+		height: "54.55",
+		price: "6543",
+		phone: "83434333",
+		elements: "7",
+	},
+	{
+		image: "https://via.placeholder.com/500x500/cccccc/615f5f?text=Why",
+		width: "43.43",
+		height: "54.55",
+		price: "5664",
+		phone: "83434333",
+		elements: "7",
+	},
+	{
+		image: "https://via.placeholder.com/500x500/cccccc/615f5f?text=Something",
+		width: "43.43",
+		height: "54.55",
+		price: "3644",
+		phone: "83434333",
+		elements: "20",
+	},
+]
 
-fetch('http://localhost:5501/')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-    insertCards(data)
-  })
-  .catch(error => {
-    console.error('Error fetching JSON: ', error);
-  });
+fetch("http://localhost:5501/api/")
+	.then((response) => response.json())
+	.then((data) => {
+		console.log(data)
+		insertCards(data)
+	})
+	.catch((error) => {
+		console.error("Error fetching JSON: ", error)
+	})
 
-
-function createCard({image, width, height, price, phone, elements}){
-    const card = `
+function createCard({ image, width, height, price, phone, elements }) {
+	const card = `
 
 <div class="card flex-basis-25">
                 <img src="${image}" class="card flex-basis-25-img-top" alt="">
@@ -73,20 +71,17 @@ function createCard({image, width, height, price, phone, elements}){
                         </div>
                     </div>
                     
-                    
-                    
                 </div>
             </div>
 
 `
-    return card
+	return card
 }
+function insertCards(data) {
+	const container = document.getElementById("cards-container")
 
-function insertCards(data){
-    const container = document.getElementById('cards-container');
-
-    data.forEach((element)=>{
-        const card = createCard(element)
-        container.insertAdjacentHTML('beforeend', card)
-    })
+	data.forEach((element) => {
+		const card = createCard(element)
+		container.insertAdjacentHTML("beforeend", card)
+	})
 }
--- server/server.js
+++ server/server.js
@@ -1,83 +1,93 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const fs = require('fs');
-const {Database} = require('./database');
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const fs = require("fs")
+const { Database } = require("./database")
+const path = require("path")
 
-const app = express();
+const app = express()
 
 const db = new Database()
 
 // Enable CORS
-app.use(cors());
-
-
+app.use(cors())
 
 // Parse URL-encoded request bodies (canvas image)
 
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }))
 
 // Parse JSON request bodies
-app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.json({ limit: "10mb" }))
 
 // Endpoint to get JSON
-app.post('/api/sendJSON', (req, res) => {
-  const { data } = req.body;
-  console.log('Received JSON data: ', data.modelData);
+app.post("/api/sendJSON", (req, res) => {
+	const { data } = req.body
+	console.log("Received JSON data: ", data.modelData)
 
-  saveImage(data.phone, data.image)
+	saveImage(data.phone, data.image)
 
-  db.addOrder(data.phone, data.image, data.modelData.width, data.modelData.width, data.modelData.price, data.modelData.elements)
-  getOrders()
+	db.addOrder(
+		data.phone,
+		data.image,
+		data.modelData.width,
+		data.modelData.width,
+		data.modelData.price,
+		data.modelData.elements
+	)
+	getOrders()
 
-  res.sendStatus(200);
-});
-
+	res.sendStatus(200)
+})
 
 async function getOrders() {
-    try {
-      const rows = await db.select('*', 'ITEMS');
-      console.log(rows); 
-      return rows;
-    } catch (error) {
-      console.error(error);
-    }
+	try {
+		const rows = await db.select("*", "ITEMS")
+		console.log(rows)
+		return rows
+	} catch (error) {
+		console.error(error)
+	}
 }
-function saveImage(phone, dataUrl){
-    
-    // get the data URL string from canvas.toDataURL()
-    
-    const imageData = dataUrl.split(',')[1];
-    const buffer = Buffer.from(imageData, 'base64');
-    let date = Date.now()
-    const imagePath =`${phone}/${date}.png`
+function saveImage(phone, dataUrl) {
+	// get the data URL string from canvas.toDataURL()
 
-    if(!fs.existsSync(phone)){
-        fs.mkdirSync(`${phone}`, (err)=>{
-            if (err) throw err
-        })
-    }
-    writeFile()
-    
-    function writeFile(){
-        fs.writeFileSync(imagePath, buffer, (err) => {
-            if (err) throw err;
-            console.log(`
+	const imageData = dataUrl.split(",")[1]
+	const buffer = Buffer.from(imageData, "base64")
+	let date = Date.now()
+	const imagePath = `${phone}/${date}.png`
+
+	if (!fs.existsSync(phone)) {
+		fs.mkdirSync(`${phone}`, (err) => {
+			if (err) throw err
+		})
+	}
+	writeFile()
+
+	function writeFile() {
+		fs.writeFileSync(imagePath, buffer, (err) => {
+			if (err) throw err
+			console.log(`
                 The image for phone number: ${phone} was saved
                 Image path: ${imagePath}
-                `);
-            });
-    }
-    return imagePath
+                `)
+		})
+	}
+	return imagePath
 }
 
-app.get('', async (req, res)=>{
-    console.log('request from /')
-    res.json(await getOrders())
+app.use(express.static(path.join(__dirname + "/dashboard")))
+
+app.get("/api/", async (req, res) => {
+	console.log("request from /")
+	res.json(await getOrders())
 })
+// app.get('', async (req, res)=>{
+//   console.log('request from /')
+//   res.json(await getOrders())
+// })
 
 // Start the server
 const port = 5501
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+	console.log(`Server listening on port ${port}`)
+})

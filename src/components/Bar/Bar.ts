8e9f7955d7b3a498412ff1d1c49efc29570376ee
Sergey <dungeonmir@gmail.com>
2023-06-07 00:24:44 +0300

    colors and price is now loads from server

--- src/components/Bar/Bar.ts
+++ src/components/Bar/Bar.ts
@@ -1,73 +1,70 @@
-import './bar.css'
-import Tooltip from "../UI/Tooltip/Tooltip";
-import Button from "../UI/Button/Button";
-import WidgetCanvas from '../WidgetCanvas/WidgetCanvas';
-import {fabric} from 'fabric';
-import AddTextIcon from '../../icons/text.svg'
-import TrashIcon from '../../icons/trash.svg'
-import CenterHorizontalIcon from '../../icons/center-horizontal.svg'
-import CenterVerticalIcon from '../../icons/center-vertical.svg'
-import DownloadIcon from '../../icons/download.svg'
-import Downloader from './Downloder';
+import "./bar.css"
+import Tooltip from "../UI/Tooltip/Tooltip"
+import Button from "../UI/Button/Button"
+import WidgetCanvas from "../WidgetCanvas/WidgetCanvas"
+import { fabric } from "fabric"
+import AddTextIcon from "../../icons/text.svg"
+import TrashIcon from "../../icons/trash.svg"
+import CenterHorizontalIcon from "../../icons/center-horizontal.svg"
+import CenterVerticalIcon from "../../icons/center-vertical.svg"
+import DownloadIcon from "../../icons/download.svg"
+import Downloader from "./Downloder"
 
 export default class Bar {
-    _element: HTMLDivElement
-    _downloader: Downloader
-    constructor(mountElement: HTMLDivElement, canvas: WidgetCanvas) {
-        this._element = document.createElement('div')
-        mountElement.appendChild(this._element);
-        this._element.classList.add('bar')
+	_element: HTMLDivElement
+	_downloader: Downloader
+	constructor(mountElement: HTMLDivElement, canvas: WidgetCanvas) {
+		this._element = document.createElement("div")
+		mountElement.appendChild(this._element)
+		this._element.classList.add("bar")
 
-        this._downloader = new Downloader(mountElement)
+		this._downloader = new Downloader(mountElement)
 
-        const buttons = [
-            {
-                name: 'Добавить текст',
-                func: ()=>{
-                    canvas.addText("Текст")
-                    },
-                icon: AddTextIcon,
-                
-            },
-            {
-                name: 'Выровнять по горизонтали',
-                func: ()=>{
-                    
-                    canvas._selectedObjects?.map(obj=>{
-                        canvas.alignObject(obj, true, false)
-                    })
-                },
-                icon: CenterHorizontalIcon
-            },
-            {
-                name: 'Выровнять по вертикали',
-                func: ()=>{
-                    canvas._selectedObjects?.map(obj=>{
-                        canvas.alignObject(obj, false, true)
-                    })
-                },
-                icon: CenterVerticalIcon
-            },
-            {
-                name: 'Сохранить в PNG',
-                func: () =>{
-                    this._downloader.dowload(this._downloader.render(canvas))
-                },
-                icon: DownloadIcon
-            },
-            {
-                name: 'Удалить элемент',
-                func: ()=>{
-                    canvas.deleteLastActiveObject()
-                    },
-                icon: TrashIcon,
-            }
-        ]
-        buttons.map((obj)=>{
-            const button = new Button('', obj.func, this._element)
-            button.getElement().innerHTML = obj.icon
-            new Tooltip(obj.name, button.getElement())
-        })
-    }
-    
+		const buttons = [
+			{
+				name: "Добавить текст",
+				func: () => {
+					canvas.addText("Текст")
+				},
+				icon: AddTextIcon,
+			},
+			{
+				name: "Выровнять по горизонтали",
+				func: () => {
+					canvas._selectedObjects?.map((obj) => {
+						canvas.alignObject(obj, true, false)
+					})
+				},
+				icon: CenterHorizontalIcon,
+			},
+			{
+				name: "Выровнять по вертикали",
+				func: () => {
+					canvas._selectedObjects?.map((obj) => {
+						canvas.alignObject(obj, false, true)
+					})
+				},
+				icon: CenterVerticalIcon,
+			},
+			{
+				name: "Сохранить в PNG",
+				func: () => {
+					this._downloader.dowload(this._downloader.render(canvas))
+				},
+				icon: DownloadIcon,
+			},
+			{
+				name: "Удалить элемент",
+				func: () => {
+					canvas.deleteLastActiveObject()
+				},
+				icon: TrashIcon,
+			},
+		]
+		buttons.map((obj) => {
+			const button = new Button("", obj.func, this._element)
+			button.getElement().innerHTML = obj.icon
+			new Tooltip(obj.name, button.getElement())
+		})
+	}
 }
--- src/components/ObjectProperties/ObjectProperties.ts
+++ src/components/ObjectProperties/ObjectProperties.ts
@@ -1,136 +1,163 @@
+import {
+	fonts,
+	getPriceElement,
+	getPriceTextSize,
+} from "../../resources/constants"
+import ShadowText from "../ShadowText"
+import Button from "../UI/Button/Button"
+import Tooltip from "../UI/Tooltip/Tooltip"
+import WidgetCanvas from "../WidgetCanvas/WidgetCanvas"
+import ColorPickButtons from "./ColorButtons/ColorPickButtons"
+import "./objectProperties.css"
 
-import { fonts, prices } from "../../resources/constants";
-import ShadowText from "../ShadowText";
-import Button from "../UI/Button/Button";
-import Tooltip from "../UI/Tooltip/Tooltip";
-import WidgetCanvas from "../WidgetCanvas/WidgetCanvas";
-import ColorPickButtons from "./ColorButtons/ColorPickButtons";
-import './objectProperties.css'
+export default class ObjectProperties {
+	private _element: HTMLDivElement
+	private _canvas: WidgetCanvas
+	constructor(mountElement: HTMLDivElement, canvas: WidgetCanvas) {
+		this._element = document.createElement("div")
+		this._element.classList.add("objectProperties", "hidden")
+		this._canvas = canvas
+		mountElement.appendChild(this._element)
 
-export default class ObjectProperties{
-    private _element: HTMLDivElement;
-    private _canvas: WidgetCanvas;
-    constructor(mountElement:HTMLDivElement, canvas: WidgetCanvas) {
-        this._element = document.createElement('div')
-        this._element.classList.add('objectProperties', 'hidden')
-        this._canvas = canvas
-        mountElement.appendChild(this._element)
+		canvas._canvas.on("selection:created", () => {
+			this.updateVisibility()
+		})
+		canvas._canvas.on("selection:cleared", () => {
+			this.updateVisibility()
+		})
+		canvas._canvas.on("selection:updated", () => {
+			this.updateVisibility()
+		})
 
-        canvas._canvas.on('selection:created', ()=>{this.updateVisibility()})
-        canvas._canvas.on('selection:cleared', ()=>{this.updateVisibility()})
+		this.optionText()
+		this.optionColors()
+		this.optionFonts()
+		this.optionPrice()
+	}
+	addOptionDiv() {
+		const optionDiv = document.createElement("div")
+		optionDiv.classList.add("option")
+		return optionDiv
+	}
+	addOption(labelText: string = "") {
+		const label = document.createElement("div")
+		label.classList.add(labelText)
+		label.classList.add("label")
+		this._element.appendChild(label)
+		const option = this.addOptionDiv()
+		label.appendChild(option)
 
-        this.optionText()
-        this.optionColors()
-        this.optionFonts()
-        this.optionPrice()
+		return option
+	}
+	addRange(
+		min: string,
+		max: string,
+		step: string,
+		mountElement: HTMLDivElement
+	) {
+		const range = document.createElement("input")
+		range.type = "range"
+		range.min = min
+		range.max = max
+		range.step = step
+		mountElement.appendChild(range)
+	}
+	updateVisibility() {
+		this._element.classList.toggle("hidden")
 
-    }
-    addOptionDiv(){
-        const optionDiv = document.createElement('div')
-        optionDiv.classList.add('option')
-        return optionDiv
-    }
-    addOption(labelText: string = ''){
-        
-       
-        const label = document.createElement('div')
-        label.classList.add(labelText)
-        label.classList.add('label')
-        this._element.appendChild(label)
-        const option = this.addOptionDiv()
-        label.appendChild(option)
+		this.showPrice()
+	}
+	optionText() {
+		const optionElement = this.addOption("Текст")
+		const input = document.createElement("textarea")
+		input.classList.add("textarea")
+		input.placeholder = "Ваш текст"
+		input.rows = 3
+		optionElement.appendChild(input)
+		input.oninput = (e) => {
+			const value = (e.target as HTMLTextAreaElement).value
+			this._canvas._selectedObjects.map((obj: ShadowText) => {
+				obj.changeText(value)
+			})
+			this._canvas.update()
+		}
+	}
+	optionColors() {
+		const optionElement = this.addOption("Цвет")
+		const colorPickButtons = new ColorPickButtons(this._canvas)
+		optionElement.appendChild(colorPickButtons.element)
+	}
 
-        return option
-    }
-    addRange(min:string, max:string, step: string, mountElement: HTMLDivElement){
-        const range = document.createElement('input')
-        range.type = 'range'
-        range.min = min
-        range.max = max
-        range.step = step
-        mountElement.appendChild(range)
-    }
-    updateVisibility(){
-        this._element.classList.toggle('hidden')
+	optionFonts() {
+		const option = this.addOption("Шрифт")
+		option.classList.add("fontOption")
+		fonts.map((font) => {
+			const button = new Button(
+				font,
+				() => {
+					this._canvas._selectedObjects.map((obj: ShadowText) => {
+						obj.changeFont(font)
+					})
+					this._canvas.update()
+					this.showPrice()
+				},
+				option
+			)
+			button.getElement().classList.add("fontOptionButton")
+			button.getElement().style.fontFamily = font
+			button.getElement().style.fontSize = "1em"
+		})
+	}
+	changeShadow(color: string) {
+		this._canvas._selectedObjects.map((obj: ShadowText) => {
+			obj.changeShadow(color)
+			obj.dirty = true
+		})
+		this._canvas.update()
+	}
+	optionPrice() {
+		const option = this.addOption("Стоимость")
+		const priceDiv = document.createElement("div")
+		priceDiv.classList.add("priceDiv")
+		priceDiv.textContent = ""
+		option.appendChild(priceDiv)
 
-        this.showPrice()
-    }
-    optionText(){
-        const optionElement = this.addOption('Текст')
-        const input = document.createElement('textarea')
-        input.classList.add('textarea')
-        input.placeholder = 'Ваш текст'
-        input.rows = 3
-        optionElement.appendChild(input) 
-        input.oninput = (e)=>{
-            const value = (e.target as HTMLTextAreaElement).value
-            this._canvas._selectedObjects.map((obj: ShadowText)=>{
-                obj.changeText(value)
-            })
-            this._canvas.update()
-            
-        }
-    }
-    optionColors(){
-        
-    const optionElement = this.addOption('Цвет')
-    const colorPickButtons = new ColorPickButtons(this._canvas)
-    optionElement.appendChild(colorPickButtons.element)
+		this._canvas._canvas.on("object:modified", () => {
+			this.showPrice()
+		})
+	}
+	showPrice() {
+		const selection = this._canvas.selectAll()
+		selection.ungroupOnCanvas()
+		let price = 0
+		let elements = 0
+		let width = Number.parseFloat(
+			(
+				Math.round((selection.width + Number.EPSILON) * 100) / 100
+			).toString()
+		)
+		let height = Number.parseFloat(
+			(
+				Math.round((selection.height + Number.EPSILON) * 100) / 100
+			).toString()
+		)
 
-    }
+		selection.getObjects().map((obj) => {
+			if (obj instanceof ShadowText) {
+				elements += obj.getText().trim().replace(" ", "").length
+			}
+		})
+		price =
+			width * getPriceTextSize() +
+			height * getPriceTextSize() +
+			elements * getPriceElement()
+		price = Math.floor(price)
+		const priceDiv = document.querySelector(".priceDiv")
+		price && (priceDiv.textContent = price?.toString() + " р.")
 
-    optionFonts(){
-        const option = this.addOption('Шрифт')
-        option.classList.add('fontOption')
-        fonts.map((font)=>{
-            const button = new Button(font, ()=>{
-                this._canvas._selectedObjects.map((obj:ShadowText)=>{
-                    obj.changeFont(font)
-                })
-                this._canvas.update()
-            }, option)
-            button.getElement().classList.add('fontOptionButton')
-            button.getElement().style.fontFamily = font
-            button.getElement().style.fontSize = '1em'
-        })
-       
-    }
-    changeShadow(color: string ){
-        this._canvas._selectedObjects.map((obj: ShadowText)=>{
-            obj.changeShadow(color)
-            obj.dirty = true
-        })
-        this._canvas.update()
-    }
-    optionPrice(){
-        const option = this.addOption('Стоимость')
-        const priceDiv = document.createElement('div')
-        priceDiv.classList.add('priceDiv')
-        priceDiv.textContent =""
-        option.appendChild(priceDiv)
-
-        this._canvas._canvas.on('object:modified', ()=>{
-            this.showPrice()
-        })
-    }
-    showPrice(){
-        const selection = this._canvas.selectAll()
-        selection.ungroupOnCanvas()
-        let price = 0
-        let elements = 0
-        let width  = Number.parseFloat((Math.round((selection.width + Number.EPSILON) * 100) / 100).toString())
-        let height =  Number.parseFloat((Math.round((selection.height + Number.EPSILON) * 100) / 100).toString())
-        
-        selection.getObjects().map(obj=>{
-            if(obj instanceof ShadowText){
-                elements += obj.getText().trim().replace(' ', '').length
-               }
-        })
-        price = (width * prices.price1mm) + (height * prices.price1mm) + elements * prices.priceforElement
-        
-        const priceDiv = document.querySelector('.priceDiv')
-        price && ( priceDiv.textContent =price?.toString() + ' р.')
-        
-        const tooltipPrice = new Tooltip('Примерная стоимость вывески', priceDiv as HTMLDivElement)
-    }
+		const tooltipPrice = new Tooltip(
+			"Примерная стоимость вывески",
+			priceDiv as HTMLDivElement
+		)
+	}
 }
